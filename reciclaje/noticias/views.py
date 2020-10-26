392e7552cf6dd4cc9c919b2a9668469b192cf146
reciclate <reciclate.esta@gmail.com>
2020-10-26 17:54:56 -0300

    resto del sitio funcional con django

--- reciclaje/noticias/views.py
+++ reciclaje/noticias/views.py
@@ -3,3 +3,14 @@ from django.shortcuts import render
 def inicio(request):
     return render(request, 'noticias/index.html', {})
 
+def nosotros(request):
+    return render(request, 'noticias/nosotros.html', {})
+
+def lista_noticias(request):
+    return render(request, 'noticias/noticias.html', {})
+
+def proyectos(request):
+    return render(request, 'noticias/proyectos.html', {})
+
+def contacto(request):
+    return render(request, 'noticias/contacto.html', {})
