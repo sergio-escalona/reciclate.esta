2d3679a0f155ee2785f67c73c85dba750d567c05
reciclate <reciclate.esta@gmail.com>
2020-11-03 16:47:21 -0300

    Creacion prueba unitaria

---
+++ reciclaje/noticias/test_prueba.py
@@ -0,0 +1,16 @@
+import unittest
+from reciclaje.noticias.models import Contacto, Noticias
+from django.utils import timezone
+from django.http import request
+from .prueba import listar, contacto
+
+
+
+class TestProbar(unittest.TestCase):
+    def test_listar(self):
+        self.assertEqual(listar(1),1)
+        self.assertEqual(listar(0),1)
+
+    def test_contacto(self):
+        self.assertTrue(contacto('Juana', 'juana@gmail.com', 123345678, 'Hola', 2, False))
+        self.assertFalse(contacto('Romia', 'romina@gmail.com', 'Sin numero', 'Chao', 1, True))
